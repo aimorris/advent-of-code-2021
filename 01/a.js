d76dd47dcb025e91a0dec2c7f0ea5ed3710729c1
Adam Morris <aimorris@aimorris.com>
2021-12-16 13:30:14 +1000

    refactor b

--- 01/a.js
+++ 01/a.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 fs.readFile(process.argv[2], function(err, f) {
   if (err) return console.error(err)
 
-  const depths = f.toString().split('\n').map(n => parseInt(n))
+  const depths = f.toString().split('\n').map(n => parseInt(n)).filter(x => !isNaN(x))
 
   console.log(depths.filter((_, i) => depths[i] > depths[i-1]).length)
 })
