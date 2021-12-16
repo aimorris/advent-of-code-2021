4f6aca01db7a13df2ffe2a44f61eef81214ceb81
Adam Morris <aimorris@aimorris.com>
2021-12-16 13:19:08 +1000

    refactor 01-a

--- 01/a.js
+++ 01/a.js
@@ -1,14 +1,9 @@
 const fs = require('fs')
-let increases = 0
 
 fs.readFile(process.argv[2], function(err, f) {
   if (err) return console.error(err)
 
   const depths = f.toString().split('\n').map(n => parseInt(n))
 
-  for (let i = 1; i < depths.length; i++) {
-    if (depths[i] > depths[i-1]) increases++
-  }
-
-  console.log(increases)
+  console.log(depths.filter((_, i) => depths[i] > depths[i-1]).length)
 })
